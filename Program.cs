9c45444e2a4bd1f52c8de5e89c8f701e5696aa02
Entra21 <entra21@proway.treina>
2020-07-07 21:03:34 -0300

    texto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace segunda__aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var myBoolean= true;
+
+            Console.WriteLine("Você é maior de idade?");
+            var result = Console.ReadLine();
+
+            myBoolean = result== "sim";
+            Console.WriteLine(myBoolean );
+            
+        }
+    }
+}
