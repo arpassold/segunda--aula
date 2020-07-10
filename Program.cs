86b21bf12d8c5170e71ffe3de51243b9881fab38
Entra21 <entra21@proway.treina>
2020-07-09 21:36:15 -0300

    readme

--- Program.cs
+++ Program.cs
@@ -6,13 +6,15 @@ namespace segunda__aula
     {
         static void Main(string[] args)
 
+      
+
         
         { 
             Console.WriteLine("Qual sua idade?");
           var result = Console.ReadLine();
 
              //Transformando o texto que o usuário digitou  para um numero
-            var userAge = Int32.Parse(result);
+            int userAge = Int32.Parse(result);
           
 
             // comparando se a idade do usuário é maior que 15 ou menos que 51)
@@ -21,7 +23,12 @@ namespace segunda__aula
             {  
                  Console.WriteLine( "sim!Você tem idade para o entra 21");
             }
-            else
+        //    else if(userAge==14|| userAge==15)
+        //     {
+        //         //Coparando SE o valor da variavel não for true vai cair aqui                text = "Não! Eu sou menor de idade";
+        //         Console.WriteLine( "não, você não tem idade para o entra 21, mas pode ser menor prendiz");
+        //          }
+           else 
             {
                 //Coparando SE o valor da variavel não for true vai cair aqui                text = "Não! Eu sou menor de idade";
                 Console.WriteLine( "não, você não tem idade para o entra 21");
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto Entra21
+
